8e6137ca39ff9d8fdb4fce86f4d0a30ec77fac84
Brian Dunnette <brian@dunnette.us>
2010-12-18 05:42:22 -0600

    minor formatting changes

--- __tryton__.py
+++ __tryton__.py
@@ -1,18 +1,16 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
 {
-    'name': 'Unified Chart of Accounts for United States Non-Profit Organizations',
-    'version': '1.4.0',
-    'author': 'Brian Dunnette',
+    "name" : "Unified Chart of Accounts for United States Non-Profit Organizations",
+    "version" : "1.4.0",
+    "author" : "Brian Dunnette",
     'email': 'brian@dunnette.us',
     'website': 'http://freegeektwincities.org',
-    'category': 'Accounting',
-    'description': '''Defines an account template for US non-profit/charitable organizations, based on work from NCCS/Urban Institute: http://nccs.urban.org/projects/ucoa.cfm
-''',
-    'depends': [
-        'account',
+    "category" : "Accounting",
+    "description": "Defines an account template for US non-profit/charitable organizations, based on work from NCCS/Urban Institute: http://nccs.urban.org/projects/ucoa.cfm",
+    "depends" : [
+        "account",
     ],
-    'xml': [
-        'account_ucoa.xml',
+    "xml" : [
+        "account_ucoa.xml",
     ],
 }
+
