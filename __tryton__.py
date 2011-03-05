0038ff5948c79f69bc4d9957b43c73069d4ff91b
Brian Dunnette <brian@dunnette.us>
2011-03-05 07:55:06 -0600

    bumping version number to 1.8 based on OpenERP 5's ucoa module

--- __tryton__.py
+++ __tryton__.py
@@ -1,6 +1,6 @@
 {
     "name" : "Unified Chart of Accounts for US Non-Profits",
-    "version" : "1.4.0",
+    "version" : "1.8.0",
     "author" : "Free Geek Twin Cities",
     'email': 'brian@freegeektwincities.org',
     'website': 'http://tryton-ucoa.googlecode.com/',
